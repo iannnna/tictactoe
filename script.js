da6753a907cd83046f53d5bd1a58c46efe090913
iannnna <julianna.barrera03@gmail.com>
2023-04-11 19:59:50 +0800

    previousGameFunc

--- script.js
+++ script.js
@@ -5,9 +5,16 @@ let board = [
 ];
 let currentPlayer = "X";
 let reviewingGame = false;
+let moveHistory = [];
 
 function makeMove(row, col) {
   if (!reviewingGame && board[row][col] === "") {
+    let move = {
+      player: currentPlayer,
+      row: row,
+      col: col,
+    };
+    moveHistory.push(move);
     board[row][col] = currentPlayer;
     document.getElementById("tic-tac-toe-board").children[row].children[
       col
@@ -93,6 +100,7 @@ function isBoardFull() {
 
 function resetGame() {
   reviewingGame = false;
+  moveHistory = [];
   board = [
     ["", "", ""],
     ["", "", ""],
@@ -103,6 +111,7 @@ function resetGame() {
   for (let i = 0; i < cells.length; i++) {
     cells[i].innerText = "";
   }
+  document.getElementById("previousMoveButton").disabled = true;
 }
 
 function restartGame() {
@@ -114,4 +123,20 @@ function restartGame() {
 function reviewGame() {
   reviewingGame = true;
   document.getElementById("myModal").style.display = "none";
+  document.getElementById("previousMoveButton").disabled = false;
+}
+
+function previousMove() {
+  if (moveHistory.length > 0) {
+    let move = moveHistory.pop();
+    let { player, row, col } = move;
+    board[row][col] = "";
+    document.getElementById("tic-tac-toe-board").children[row].children[
+      col
+    ].innerText = "";
+    currentPlayer = player;
+    if (moveHistory.length === 0) {
+      document.getElementById("previousMoveButton").disabled = true;
+    }
+  }
 }
